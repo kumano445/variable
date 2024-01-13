11325ef15c2d7dda5ced0d4436c80cb02a6dc534
yoshiko <ttkmysk021@gmail.com>
2024-01-13 12:28:02 +0900

    first commit

---
+++ demo_1_variable/src/demo_1_variable/variable.java
@@ -0,0 +1,16 @@
+package demo_1_variable;
+
+public class variable {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int num1 = 5;
+		double Value = 3.17;
+		String myText = "武隈";
+		
+		System.out.println("整数は" + num1 +  "小数点は" + Value + "名前は" + myText);
+		
+		
+	}
+
+}
